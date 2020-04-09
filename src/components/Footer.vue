f09c4085a451f2c4896b33bcea2851a74b12c73c
Jivko Valkanov <jivko@edatachase.com>
2020-04-09 17:42:05 +0300

    changed footer;

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <v-footer class="pa-3" color="primary" app>
-        <span class="white--text"> eDatachase Ltd. &copy; 2019</span>
+        <span class="white--text"> Unknown Ltd. &copy; 2019</span>
         <v-spacer></v-spacer>           
         <span class="white--text">Version {{version}}</span>
     </v-footer> 
